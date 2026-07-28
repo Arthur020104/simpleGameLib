59b8688add460ce496ee7283f3c198a47c1c3f48
Arthur020104 <arthur020104@gmail.com>
2026-07-28 18:21:54 -0300

    [UPDATE] Making ray travel to model space for proper intersection detection

--- gameObject.cpp
+++ gameObject.cpp
@@ -51,5 +51,25 @@ void GameObject::draw(cy::Matrix4f &projection, cy::Matrix4f &view)
 
 bool GameObject::intersect(Ray& ray)
 {
-  return this->mesh->intersectMesh(ray, this);
+  //the ray vector will be transformed to the object space
+  cy::Matrix4f invertedModelMatrix = this->getModelMatrix().GetInverse();
+
+  cy::Vec4f modelSpaceOrigin = invertedModelMatrix * cy::Vec4f(ray.origin, 1.0f);
+  cy::Vec4f modelSpaceDirection = invertedModelMatrix * cy::Vec4f(ray.direction, 0.0f);
+  
+  float scale = modelSpaceDirection.XYZ().Length();
+
+  Ray modelSpaceRay;
+  modelSpaceRay.origin = modelSpaceOrigin.XYZ();
+  modelSpaceRay.direction = modelSpaceDirection.XYZ() / scale;
+
+  if(!this->mesh->intersectMesh(modelSpaceRay, this)) return false;
+
+  for(Hit& hit : modelSpaceRay.hits)
+  {
+    hit.point = (this->getModelMatrix() * cy::Vec4f(hit.point, 1.0f)).XYZ();
+    ray.hits.push_back(hit);
+  }
+  
+  return true;
 }
